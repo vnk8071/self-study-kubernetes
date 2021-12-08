fe8b20579165baf967929d1db5f6f011d973cfc4
vnk8071 <nguyenkhoi8071@gmail.com>
2021-12-08 22:08:12 +0700

    ADD script to setup for linux system

---
+++ setup-linux.sh
@@ -0,0 +1,7 @@
+wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+chmod +x minikube-linux-amd64
+sudo mv minikube-linux-amd64 /usr/local/bin/minikube
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
